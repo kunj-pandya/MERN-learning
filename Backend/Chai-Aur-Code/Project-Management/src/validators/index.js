4cf1213f36ddf0ff2429364c97c5bff446b577df
kunj-pandya <kunjpandya998@gmail.com>
2025-09-19 21:00:26 +0530

    feat(validators): add validators for password change, forgot password, and reset password

--- Backend/Chai-Aur-Code/Project-Management/src/validators/index.js
+++ Backend/Chai-Aur-Code/Project-Management/src/validators/index.js
@@ -40,9 +40,40 @@ const userLoginValidator = () => {
     ];
 }
 
+const userChangedCurrentPasswordValidator = () => {
+    return [
+        body("oldPassword")
+            .notEmpty()
+            .withMessage("Old password is required"),
+        body("newPassword")
+            .notEmpty()
+            .withMessage("New password is required"),
+    ];
+};
 
+const userForgotPasswordValidator = () => {
+    return [
+        body("email")
+            .notEmpty()
+            .withMessage("Email is required")
+            .isEmail()
+            .withMessage("Email is invalid")
+    ];
+};
+
+const userResetForgotPasswordValidator = () => {
+    return [
+        body("newPassword")
+            .notEmpty()
+            .withMessage("Password is required")
+    ];
+};
 
 
 export {
-    userRegisterValidator, userLoginValidator
+    userRegisterValidator,
+    userLoginValidator,
+    userChangedCurrentPasswordValidator,
+    userForgotPasswordValidator,
+    userResetForgotPasswordValidator
 }
