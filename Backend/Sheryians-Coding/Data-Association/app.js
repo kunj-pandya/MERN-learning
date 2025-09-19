94daddbe31bb5b7b73855d000c7466443ad92c80
kunj-pandya <kunjpandya998@gmail.com>
2025-09-19 17:28:26 +0530

    add /like route for like feature for post

--- Backend/Sheryians-Coding/Data-Association/app.js
+++ Backend/Sheryians-Coding/Data-Association/app.js
@@ -64,7 +64,20 @@ app.post("/login", async (req, res) => {
 app.get("/profile", isLoggedIn, async (req, res) => {
     let user = await userModel.findOne({ email: req.user.email }).populate("posts");
     res.render("profile", { user });
-})
+});
+
+app.get("/like/:id", isLoggedIn, async (req, res) => {
+    let post = await postModel.findOne({ _id: req.params.id }).populate("user");
+
+    if (post.likes.indexOf(req.user.userid) === -1) {
+        post.likes.push(req.user.userid);
+    }
+    else {
+        post.likes.splice(post.likes.indexOf(req.user.userid), 1);
+    }
+    await post.save();
+    res.redirect("/profile");
+});
 
 app.post("/post", isLoggedIn, async (req, res) => {
     let user = await userModel.findOne({ email: req.user.email });
