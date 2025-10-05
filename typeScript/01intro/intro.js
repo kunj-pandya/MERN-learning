54cbe3d3825eda073f76bc08180fc0a936fcdd2c
kunj-pandya <kunjpandya998@gmail.com>
2025-10-05 19:32:35 +0530

    add javscript code

---
+++ typeScript/01intro/intro.js
@@ -0,0 +1,4 @@
+var user = { name: "kunj", age: 24 };
+console.log("kunj");
+console.log(user.name);
+// console.log(user.email); //static checking
---
+++ typeScript/01intro/intro.ts
@@ -0,0 +1,5 @@
+let user = { name: "kunj", age: 24 }
+
+console.log("kunj");
+console.log(user.name);
+// console.log(user.email); //static checking
