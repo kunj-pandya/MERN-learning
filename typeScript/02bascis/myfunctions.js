3e13f0eedb7a85eb40d6bdad48a03cf9554a9983
kunj-pandya <kunjpandya998@gmail.com>
2025-10-06 16:25:35 +0530

    naver and void in functions

--- typeScript/02bascis/myfunctions.js
+++ typeScript/02bascis/myfunctions.js
@@ -3,6 +3,10 @@ Object.defineProperty(exports, "__esModule", { value: true });
 function addTwo(num) {
     return num + 2;
 }
+// function addTwo(num: number) {
+// retrun "hello"
+// }
+// let myValue = addTwo(5);  //allowed but we should not do this
 function geteUpper(val) {
     return val.toUpperCase();
 }
@@ -14,3 +18,23 @@ addTwo(5);
 // addTwo("3"); //not allowed
 geteUpper("kunj");
 signUpUser("kunj", "kunj@gmail.com", false);
+loginUser("kunj", "kunj@gmail.com");
+// Arrow function
+var getHello = function (s) {
+    return "";
+};
+// Arrays
+var heros = ["thor", "spiderman", "tronman"];
+heros.map(function (hero) {
+    return "hero is ".concat(hero);
+});
+// void
+// void represents the return value of functions which don’t return a value. It’s the inferred type any time a function doesn’t have any return statements, or doesn’t return any explicit value from those return statements
+function consoleError(errmsg) {
+    console.log(errmsg);
+}
+// naver
+// The never type represents values which are never observed. In a return type, this means that the function throws an exception or terminates execution of the program.
+function handleError(errmsg) {
+    throw new Error(errmsg);
+}
