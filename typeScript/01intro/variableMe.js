14a23de0fc255579df4d570ab0af6a1f83a8206a
kunj-pandya <kunjpandya998@gmail.com>
2025-10-06 09:38:20 +0530

    typescript veriables

---
+++ typeScript/01intro/variableMe.js
@@ -0,0 +1,15 @@
+"use strict";
+// -------- Syntex ----------
+// let variable: type = value; //value: always in lowercase
+Object.defineProperty(exports, "__esModule", { value: true });
+// ------ Example ----------
+var greetings = "Hello kunj";
+// greetings = 6;  //Type 'number' is not assignable to type 'string'.
+var mynum = 6;
+// mynum.toUpperCase();  //String methods can'be used for number
+console.log(greetings);
+// number
+var userId = 112233;
+// userId = "kunj" //can't do 
+// boolean
+var isLoggedIn = false;
---
+++ typeScript/01intro/variableMe.ts
@@ -0,0 +1,24 @@
+// -------- Syntex ----------
+// let variable: type = value; //value: always in lowercase
+
+// ------ Example ----------
+let greetings: string = "Hello kunj";
+// greetings = 6;  //Type 'number' is not assignable to type 'string'.
+
+let mynum = 6;
+// mynum.toUpperCase();  //String methods can'be used for number
+
+console.log(greetings);
+
+
+// number
+
+let userId = 112233;
+// userId = "kunj" //can't do 
+
+// boolean
+let isLoggedIn : boolean = false;
+
+
+
+export { }
